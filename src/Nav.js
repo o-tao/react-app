7bf1298519e552d238d8fdf18020486a5dbe1860
o-tao <dhtkdals369@gmail.com>
2024-09-12 10:51:48 +0900

    주석 변경

--- src/Nav.js
+++ src/Nav.js
@@ -11,9 +11,12 @@ const Nav = props => {
 
     return (
         <nav>
-            {/*<a href={"#page1"} onClick={() => {alert('PAGE1');}}>PAGE1</a> /!* 방식1 : 값을 직접 넣어 사용 *!/*/}
-            {/*<a href={"#page2"} onClick={pageEvent}>PAGE2</a> /!* 방식2 : 메서드 함수 불러와서 사용 *!/*/}
-            {/*<a href={"#page3"} onClick={() => {data('PAGE3');}}>PAGE3</a> /!* 방식3 : 메서드 함수 불러와서 사용 (내용 직접 입력) *!/*/}
+            {/* 방식1 : 값을 직접 넣어 사용 */}
+            {/*<a href={"#page1"} onClick={() => {alert('PAGE1');}}>PAGE1</a>*/}
+            {/* 방식2 : 메서드 함수 불러와서 사용 */}
+            {/*<a href={"#page2"} onClick={pageEvent}>PAGE2</a>*/}
+            {/* 방식3 : 메서드 함수 불러와서 사용 (내용 직접 입력) */}
+            {/*<a href={"#page3"} onClick={() => {data('PAGE3');}}>PAGE3</a>*/}
 
             {/* 방식4 : props 사용 */}
             {/*<a href={"#page1"} onClick={() => props.pEvent('PAGE1')}>PAGE1</a>*/}
