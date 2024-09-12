39a97e12d598e409f1bd677a1d6f2674bde9bcca
o-tao <dhtkdals369@gmail.com>
2024-09-12 11:16:05 +0900

    Hooks useState 사용 예제

--- src/App.js
+++ src/App.js
@@ -3,19 +3,33 @@ import Nav from './Nav';
 import Page1 from './Page1';
 import Page2 from './Page2';
 import Page3 from './Page3';
+import {useState} from 'react'
 
-const html =
-    <>
-        <Page1/>
-        <Page2/>
-        <Page3/>
-    </>;
+// const html =
+//     <>
+//         <Page1/>
+//         <Page2/>
+//         <Page3/>
+//     </>;
 
 const App = () => {
+    let [page, setPage] = useState(<Page1/>);
+
+    const pageEven = txt => {
+        console.log(txt);
+        if (txt === "PAGE1") {
+            setPage(<Page1/>);
+        } else if (txt === "PAGE2") {
+            setPage(<Page2/>);
+        } else if (txt === "PAGE3") {
+            setPage(<Page3/>);
+        }
+    }
+
     return (
         <>
-            <Nav pEvent={txt=>alert(txt)}/>
-            {html}
+            <Nav pEvent={txt => pageEven(txt)}/>
+            {page}
         </>
     );
 }
