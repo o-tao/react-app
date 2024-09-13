a5132ec13cc1e8cf5c3ca612b11f171e23ce1312
o-tao <dhtkdals369@gmail.com>
2024-09-13 10:44:07 +0900

    Hooks을 이용하여 localStorage에 저장된 data 화면출력 예제

--- src/board/List.js
+++ src/board/List.js
@@ -1,10 +1,20 @@
-const List = () => {
-    const array = [
-        {id: 1, title: "제목1"},
-        {id: 2, title: "제목2"}
-    ];
+import {useState, useEffect} from "react";
 
-    localStorage.setItem("Board", JSON.stringify(array));
+const List = () => {
+    /* localStorage에 저장 */
+    // const array = [
+    //     {id: 1, title: "제목1"},
+    //     {id: 2, title: "제목2"}
+    // ];
+    // localStorage.setItem('data', JSON.stringify(array));
+
+    /* [Hooks] localStorage에 저장된 data 화면에 출력 */
+    const [array, setArray] = useState([]);
+    useEffect(() => {
+        const resultString = localStorage.getItem('data');
+        const result = JSON.parse(resultString);
+        setArray(result);
+    }, []);
 
     return (
         <div className={"container"} style={{'marginTop': '80px'}}>
