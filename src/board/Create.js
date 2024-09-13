8741fe0663d159414e386eb0f8844b2ffe98a986
o-tao <dhtkdals369@gmail.com>
2024-09-13 11:03:41 +0900

    insert 시 기존 localStorage에 data 추가

--- src/board/Create.js
+++ src/board/Create.js
@@ -1,15 +1,32 @@
+import {useState, useEffect} from "react";
 import {useNavigate} from 'react-router-dom';
 
 const Create = () => {
     const navigate = useNavigate();
+
+    /* [Hooks] localStorage에 저장된 data 화면에 출력 */
+    const [array, setArray] = useState([]);
+    useEffect(() => {
+        const resultString = localStorage.getItem('data');
+        const result = JSON.parse(resultString);
+        setArray(result);
+    }, []);
+
     const submitEvent = e => {
         e.preventDefault();
+
         let params = {
+            id: (array.length + 1), // insert 시 id값 +1
             title: e.target.board_title.value,
             content: e.target.board_content.value
         }
-        console.log(params);
-        navigate("/detail/1")
+
+        console.log(params, array, [...array, params]);
+
+        const newArray = [...array, params];
+        localStorage.setItem('data', JSON.stringify(newArray));
+
+        // navigate("/detail/1")
     }
     return (
         <>
@@ -21,7 +38,7 @@ const Create = () => {
                     <input type={"text"} className={"form-control"} name={"board_title"} placeholder={"글을 작성하세요."}/>
                     <div className={"mb-3 mt-3"}>
                         <label htmlFor={"board_content"}>작성글 내용:</label>
-                        <textarea className={"form-control"} rows={"5"} name={"board_content"}
+                        <textarea className={"form-control"} rows="5" name={"board_content"}
                                   placeholder={"글 내용을 작성하세요."} style={{resize: 'none'}}></textarea>
                     </div>
                     <div className={"btn-group"}>
