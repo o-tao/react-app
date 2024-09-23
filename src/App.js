c45accd544889a07ea14ff2d7f15f130254c7247
o-tao <dhtkdals369@gmail.com>
2024-09-23 11:28:55 +0900

    React 기본 페이지 적용

--- src/App.js
+++ src/App.js
@@ -1,29 +1,25 @@
+import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import AppPage from "./AppPage";
 
 function App() {
-    let a = {textAlign: "center"};
-
-    return (
-        <>
-            <h1 className={""} style={a}>Hello World</h1>
-
-            <div className={"nav"}>
-                <a href={"/"}>홈</a>
-                <a href={"/Page/"}>페이지 이동</a>
-            </div>
-
-            <BrowserRouter>
-                <Routes>
-                    {/*<Route path={"/Page"} element={<Page/>}></Route>*/}
-                    <Route path={"/Page/*"} element={<AppPage/>}></Route>
-                </Routes>
-            </BrowserRouter>
-
-            <h2 style={a}>footer</h2>
-        </>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
