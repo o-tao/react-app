db9d6577e6fae36334e21671e716547212e8758c
o-tao <dhtkdals369@gmail.com>
2024-09-23 09:54:02 +0900

    css 적용 예제1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
+import App from './App';
 // import Study from './study/Study01';
-import Board from './board/Board';
+// import Board from './board/Board';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/*<App />*/}
+    <App />
     {/*<Study />*/}
-    <Board />
+    {/*<Board />*/}
   </React.StrictMode>
 );
 
