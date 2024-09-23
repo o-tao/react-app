892395b30c71e86b2524729b796b41d5c502137a
o-tao <dhtkdals369@gmail.com>
2024-09-23 10:08:48 +0900

    Router 페이지 이동 예제

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import './App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import AppPage from "./AppPage";
 
 function App() {
-  let a = {textAlign: "center", fontSize: "20px"};
-  return (
-      <>
-        <h1 className={""} style={a}>안녕하세요</h1>
-      </>
-  );
+    let a = {textAlign: "center", fontSize: "20px"};
+
+    return (
+        <>
+            <h1 className={""} style={a}>안녕하세요</h1>
+            {/*<a href={"/page.html"}></a>*/}
+            {/*<AppPage/>*/}
+
+            <a href={"/AppPage"}>페이지 이동</a>
+
+            <BrowserRouter>
+                <Routes>
+                    <Route path={"/AppPage"} element={<AppPage/>}></Route>
+                </Routes>
+            </BrowserRouter>
+
+            <h2>footer</h2>
+        </>
+    );
 }
 
 export default App;
---
+++ src/AppPage.js
@@ -0,0 +1,7 @@
+const AppPage = () => {
+    return (
+        <h1>Page1</h1>
+    );
+};
+
+export default AppPage;
