c2b7b23562eacfa1e8072aaf66922ea346bd8e8f
o-tao <dhtkdals369@gmail.com>
2024-09-25 10:20:18 +0900

    올린파일 조회, 전체파일리스트 조회 기능 추가

---
+++ src/files/ErrorPage.js
@@ -0,0 +1,7 @@
+const ErrorPage = () => {
+    return (
+        <h2>파일을 선택하세요.</h2>
+    );
+};
+
+export default ErrorPage;
--- src/files/File.js
+++ src/files/File.js
@@ -1,20 +1,18 @@
 import './File.css';
 import axios from "axios";
-import {useState} from "react";
+import React, {useState} from "react";
+import ErrorPage from "./ErrorPage";
 
 const File = () => {
     const host = "http://localhost:80" /* back 주소 */
-
     // let [img, setImg] = useState('');
     let [imgs, setImgs] = useState([]);
 
     const submitEvent = e => {
         e.preventDefault();
-        console.log(e.target.file.files[0]);
 
         /* file을 객체에 담아 전송해야 함 */
         const formData = new FormData();
-
         for (let i = 0; i < e.target.file.files.length; i++) {
             formData.append("file", e.target.file.files[i]);
         }
@@ -24,11 +22,11 @@ const File = () => {
             /* request 요청 */
             .then(request => {
                 console.log(request, request.data);
-
                 /* Request state 성공시 */
                 if (request.data.state) {
+                    alert("파일을 저장합니다.")
                     let viewUrl = host + "/view?url="
-                    let list = []
+                    let list = [];
 
                     for (let i = 0; i < request.data.list.length; i++) {
                         list[i] = viewUrl + request.data.list[i];
@@ -38,7 +36,22 @@ const File = () => {
                 }
             })
             /* 에러 발생 시 로그 출력 */
-            .catch(error => console.log(error));
+            .catch(error => alert("파일을 선택하세요."));
+    };
+
+    const selectEvent = () => {
+        axios.get(host + "/filesRead")
+            .then(request => {
+                if (request.data.state) {
+                    let viewUrl = host + "/view?url=";
+                    let list = [];
+                    for (let i = 0; i < request.data.list.length; i++) {
+                        list[i] = viewUrl + request.data.list[i];
+                    }
+                    setImgs(list);
+                }
+            })
+            .catch(error => alert("가져올 파일이 존재하지 않습니다."));
     };
 
     return (
@@ -46,26 +59,43 @@ const File = () => {
             <header>
                 <h1 className="text-center">파일업로드</h1>
             </header>
-            <form onSubmit={submitEvent}>
+            <main>
+
+                <form onSubmit={submitEvent}>
+                    <div className="input-body">
+                        <label form="title">그림</label>
+                        {/* input type -> file */}
+                        <input accept="image/*" autoComplete="off" name="file" type="file" multiple/>
+                    </div>
+                    <div className="input-body">
+                        <input type="submit" value="전송"/>
+                    </div>
+                </form>
+
                 <div className="input-body">
-                    <label form="title">그림</label>
-                    {/* input type -> file */}
-                    <input accept="image/*" autoComplete="off" name="file" type="file" multiple/>
+                    <button type={"button"} onClick={selectEvent}>리스트 보기</button>
                 </div>
-                <div className="input-body">
-                    <input type="submit" value="전송"/>
+
+                <div className="imgs">
+                    {
+                        // img === '' ? <></> : <img className="img" src={img} alt="test"/>
+                        /*
+                        imgs.map((row, index) => {
+                            return (
+                                <img className="img" src={row} key={index} alt="test"/>
+                            );
+                        })
+                        */
+
+                        imgs.map((row, index) => {
+                            return (
+                                imgs === null ? <ErrorPage/> :
+                                    <img src={row} className={"img"} key={index} alt={"test"}/>
+                            );
+                        })
+                    }
                 </div>
-            </form>
-            <div className="imgs">
-                {
-                    // img === '' ? <></> : <img className="img" src={img} alt="test"/>
-                    imgs.map((row, index) => {
-                        return (
-                            <img className="img" src={row} key={index} alt="test"/>
-                        );
-                    })
-                }
-            </div>
+            </main>
         </>
     );
 };
