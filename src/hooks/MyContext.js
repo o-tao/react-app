538a6a6d03807b6bd6b84fa1ce6c3b8a4a2c1edf
o-tao <dhtkdals369@gmail.com>
2024-09-26 13:03:07 +0900

    useContext 사용 예제2 - 버튼으로 스타일 변경

--- src/hooks/MyContext.js
+++ src/hooks/MyContext.js
@@ -1,41 +1,52 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import {ThemeContext} from "./useThemeContext";
 
 const Header = () => {
     const theme = useContext(ThemeContext);
-    console.log(theme.txt);
-    
+    console.log(theme.show);
+
     return (
         <header style={{
             textAlign: 'center',
-            backgroundColor: 'white',
-            color: 'black'
+            backgroundColor: theme.show ? 'black' : 'white',
+            color: theme.show ? 'white' : 'black'
         }}>
-            <h1>상단글</h1>
+            {
+                theme.show ? <h1>TRUE</h1> : <h1>FALSE</h1>
+            }
         </header>
     );
 }
 
 const Main = () => {
+    const theme = useContext(ThemeContext);
+
     return (
         <main style={{
             margin: '0',
             height: '50vh',
-            backgroundColor: 'white',
-            color: 'black'
+            backgroundColor: theme.show ? 'black' : 'white',
+            color: theme.show ? 'white' : 'black'
         }}>
+            {
+                theme.show ? <h2>변경된 스타일 입니다.</h2> : <h2>기본 스타일 입니다.</h2>
+            }
         </main>
     );
 }
 
 const Footer = () => {
+    const theme = useContext(ThemeContext);
+
     const clickEvent = () => {
+        theme.setShow(!theme.show);
     }
+
     return (
         <footer style={{
             textAlign: 'right',
-            backgroundColor: 'white',
-            color: 'black'
+            backgroundColor: theme.show ? 'black' : 'white',
+            color: theme.show ? 'white' : 'black'
         }}>
             <button type='button' onClick={clickEvent}>스타일 변경</button>
         </footer>
@@ -54,10 +65,12 @@ const Step1 = () => {
 }
 
 const MyContext = () => {
+    const [show, setShow] = useState(false);
+
     return (
         <>
             <h1 style={{textAlign: 'center'}}>useContext</h1>
-            <ThemeContext.Provider value={{txt: 'A'}}>
+            <ThemeContext.Provider value={{show, setShow}}> {/* show:show, setShow를 코드간소화 */}
                 <Step1/>
             </ThemeContext.Provider>
         </>
