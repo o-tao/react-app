85906847ebf6e359fa6116d0bfaadf4d6508f6d1
o-tao <dhtkdals369@gmail.com>
2024-09-26 11:20:08 +0900

    useEffect 예제4

--- src/hooks/MyEffect.js
+++ src/hooks/MyEffect.js
@@ -36,23 +36,33 @@ const Step3 = () => {
 }
 
 const Step4 = () => {
+    const [show, setShow] = useState(false);
+
     return (
         <>
             <button type="button" onClick={() => {
+                setShow(true);
             }}>시작
             </button>
-            <Step4sub/>
+            {show && <Step4sub/>}
         </>
     );
 }
-const Step4sub = props => {
+
+const Step4sub = props => { // props.show로 호출
+    const [num, setNum] = useState(0);
+
     useEffect(() => {
         const app = setInterval(() => {
             console.log("동작");
+            // setNum(num + 1);
+            setNum(i => i + 1);
         }, 1000);
+
         return () => clearInterval(app);
     }, []);
-    return <span style={{marginLeft: '10px'}}>0</span>
+
+    return <span style={{marginLeft: '10px'}}>{num}</span>
 }
 
 const MyEffect = () => {
