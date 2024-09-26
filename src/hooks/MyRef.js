a63da98b3d568640470287c0e9d74c2f325996dc
o-tao <dhtkdals369@gmail.com>
2024-09-26 11:34:51 +0900

    useRef 예제1

--- src/hooks/MyRef.js
+++ src/hooks/MyRef.js
@@ -1,45 +1,55 @@
-import {useState, useEffect, useRef} from 'react';
-import {Routes, Route} from 'react-router-dom';
+import {useRef} from 'react';
+import {Route, Routes} from 'react-router-dom';
 
 const Step1 = () => {
-    return <p>참조값</p>
+    const ref = useRef(1);
+    console.log(ref, ref.current);
+    return <p>{ref.current}</p>
 }
+
 const Step2 = () => {
-    const changeEvent = e => {}
+    const changeEvent = e => {
+    }
     return (
         <>
             <p>상태값 0</p>
             <p>참조값 0</p>
-            <input type='number' value="" onChange={changeEvent} />
+            <input type='number' value="" onChange={changeEvent}/>
         </>
     );
 }
 const Step3 = () => {
-    const event1 = () => {}
-    const event2 = () => {}
+    const event1 = () => {
+    }
+    const event2 = () => {
+    }
     return (
         <>
             <button type='button' onClick={event1}>상태값 변경</button>
             <span style={{marginLeft: '10px'}}>상태값 0</span>
-            <br />
+            <br/>
             <button type='button' onClick={event2}>참조값 변경</button>
             <span style={{marginLeft: '10px'}}>참조값 0</span>
         </>
     );
 }
 const Step4 = () => {
-    const event1 = () => {}
-    const event2 = () => {}
-    const event3 = () => {}
-    const event4 = () => {}
+    const event1 = () => {
+    }
+    const event2 = () => {
+    }
+    const event3 = () => {
+    }
+    const event4 = () => {
+    }
     return (
         <>
             <button type='button' onClick={event1}>변수값 변경</button>
             <span style={{marginLeft: '10px'}}>변수값 0</span>
-            <br />
+            <br/>
             <button type='button' onClick={event2}>참조값 변경</button>
             <span style={{marginLeft: '10px'}}>참조값 0</span>
-            <br />
+            <br/>
             <button type='button' onClick={event3}>상태값 변경</button>
             <button type='button' onClick={event4}>변수와 참조값 확인</button>
         </>
@@ -48,20 +58,23 @@ const Step4 = () => {
 const Step5 = () => {
     return (
         <>
-            <button type='button' onClick={()=>{}}>1증가</button>
+            <button type='button' onClick={() => {
+            }}>1증가
+            </button>
             <span style={{marginLeft: '10px'}}>상태값 : 0</span>
         </>
     );
 }
 const Step6 = () => {
-    const changeEvent = e => {}
+    const changeEvent = e => {
+    }
     const clickEvent = e => {
         e.preventDefault();
     }
     return (
         <form onSubmit={clickEvent}>
-            <input type='text' value="" onChange={changeEvent} />
-            <input type='submit' value='적용' />
+            <input type='text' value="" onChange={changeEvent}/>
+            <input type='submit' value='적용'/>
         </form>
     );
 }
@@ -79,12 +92,12 @@ const MyRef = () => {
                 <a href="/ref/6">예제6</a>
             </nav>
             <Routes>
-                <Route path="1" element={<Step1 />} />
-                <Route path="2" element={<Step2 />} />
-                <Route path="3" element={<Step3 />} />
-                <Route path="4" element={<Step4 />} />
-                <Route path="5" element={<Step5 />} />
-                <Route path="6" element={<Step6 />} />
+                <Route path="1" element={<Step1/>}/>
+                <Route path="2" element={<Step2/>}/>
+                <Route path="3" element={<Step3/>}/>
+                <Route path="4" element={<Step4/>}/>
+                <Route path="5" element={<Step5/>}/>
+                <Route path="6" element={<Step6/>}/>
             </Routes>
         </>
     );
