924c2fbe77ec8cea8d7630547caf3573cfca1f32
o-tao <dhtkdals369@gmail.com>
2024-09-27 12:01:17 +0900

    useMemo 예제1

--- src/hooks/MyMemo.js
+++ src/hooks/MyMemo.js
@@ -1,8 +1,8 @@
-import { useState, useMemo } from "react";
+import {useMemo, useState} from "react";
 
 const getData1 = num => {
     console.log("Data1 계산 시작!!");
-    for (let i = 0; i < 100000; i++) { 
+    for (let i = 0; i < 100000; i++) {
         num += i;
     }
     return num;
@@ -13,12 +13,23 @@ const getData2 = num => {
 }
 
 const Step1 = () => {
+    const [num, setNum] = useState(0);
+    const [show, setShow] = useState(false);
+    let num1 = useMemo(() => {
+        return getData1(num);
+    }, [num]);
+    let num2 = getData2(num);
     return (
         <>
-            <input type='number' value="0" onChange={e => {}}/>
-            <button type="button" onClick={() => {}}>반전효과</button>
-            <p style={{width: '100%'}}>Data1 값 : 0</p>
-            <p style={{width: '100%'}}>Data2 값 : 0</p>
+            <input type='number' value={num} onChange={e => {
+                setNum(e.target.value)
+            }}/>
+            <button type="button" onClick={() => {
+                setShow(!show)
+            }}>반전효과
+            </button>
+            <p style={{width: '100%'}}>Data1 값 : {num1}</p>
+            <p style={{width: '100%'}}>Data2 값 : {num2}</p>
         </>
     )
 }
@@ -27,7 +38,7 @@ const MyMemo = () => {
     return (
         <>
             <h1 style={{textAlign: 'center'}}>useMemo</h1>
-            <Step1 />
+            <Step1/>
         </>
     );
 }
