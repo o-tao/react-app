cd524a8ff6895d7f3160a1cb03934e57b996f551
o-tao <dhtkdals369@gmail.com>
2024-09-30 11:29:39 +0900

    axios GET, POST data 전달 예제

--- src/hooks/MyRef.js
+++ src/hooks/MyRef.js
@@ -191,9 +191,10 @@ const Step9 = () => {
         }
         console.log(params);
         // e.target.submit();
-        // axios.get("http://localhost/form1", {params})
-        //     .then(request => console.log(request))
-        //     .catch(error => console.log(error));
+
+        axios.get("http://localhost/form1", {params})
+            .then(request => console.log(request))
+            .catch(error => console.log(error));
 
         axios.post("http://localhost/form2", params)
             .then(request => console.log(request))
