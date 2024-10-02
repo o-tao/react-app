d8c49b6fe759d7f3618dda3de77b7f12103ccadb
o-tao <dhtkdals369@gmail.com>
2024-10-02 09:51:09 +0900

    View 예제1 배열 추가

--- src/viewApp/ViewApp.js
+++ src/viewApp/ViewApp.js
@@ -1,21 +1,20 @@
 import {BrowserRouter, Route, Routes, useNavigate} from 'react-router-dom';
 import './ViewApp.css';
+import {useEffect, useState} from "react";
 
 const View1 = () => {
+    const [array, setArray] = useState([]);
+    useEffect(() => {
+        const data = ["내용", "내용", "내용", "내용", "내용"];
+        setArray(data);
+    }, []);
     return (
         <div className="container">
             <h1 className="head">화면1</h1>
             <ol className="body">
-                <li>내용</li>
-                <li>내용</li>
-                <li>내용</li>
-                <li>내용</li>
-                <li>내용</li>
-                <li>내용</li>
-                <li>내용</li>
-                <li>내용</li>
-                <li>내용</li>
-                <li>내용</li>
+                {
+                    array.map((row, index) => <li key={index}>{row}</li>)
+                }
             </ol>
             <button type="button" className="more">더보기</button>
         </div>
